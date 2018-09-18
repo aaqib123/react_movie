ab0c8d28d54b04aa41d4b9aadc997b32e3a33186
Aaqib <aaqib.hyd@gmail.com>
2018-09-17 23:59:46 -0300

    first commmit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import MovieRow from './MovieRow.js'
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log("initialize!");
+
+    const movies = [
+      { id: 0, posterSrc: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_UX182_CR0,0,182,268_AL_.jpg", title: "aveners", overview: "lorem ipsum lorem ipsum lorem ipsuml orem ipsum" },
+      { id: 1, posterSrc: "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_UX182_CR0,0,182,268_AL_.jpg", title: "aveners part 2", overview: "lorem ipsum lorem ipsum lorem ipsuml orem ipsum" }
+    ]
+
+    var movieRows = [];
+
+    movies.forEach(movie => {
+      console.log(movie.id);
+      const movieRow = <MovieRow movie={movie}/> 
+      movieRows.push(movieRow);
+    })
+
+    console.log(movieRows);
+    this.state = { rows: movieRows };
+
+  }
+
+
+
+
+  render() {
+    return (
+      <div>
+        <table className="App-header">
+          <tbody>
+            <tr>
+              <td>
+                <img src="#" alt="too poor for logos" />
+              </td>
+              <td>
+                The Movie Searcher
+            </td>
+            </tr>
+          </tbody>
+        </table>
+
+        <input className="searchbox" placeholder="Enter movie name" />
+
+
+        {this.state.rows}
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/MovieRow.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class MovieRow extends React.Component {
+    render() {
+        return  (<table key={this.props.movie.id} className="movie_row">
+                <tbody>
+                    <tr>
+                        <td className="posters_row">
+                            <img alt="# " className="posters_row_img" src={this.props.movie.posterSrc} />
+                        </td>
+                        <td>
+                            {this.props.movie.title}
+                            <p>{this.props.movie.overview}</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>);
+       
+    }
+}
+
+export default MovieRow
